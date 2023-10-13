a8651d56900fe84a57f0d7b406e399cffa8ba9b8
leandrita <lea.bujh@gmail.com>
2023-10-13 11:15:07 +0200

    add sounds in js file and order buttons with css

---
+++ js/index.js
@@ -0,0 +1,39 @@
+document.getElementById('tom1').addEventListener('click', function () {
+    var audio = document.getElementById('audio-tom1');
+    audio.play();
+});
+
+document.getElementById('tom2').addEventListener('click', function () {
+    var audio = document.getElementById('audio-tom2');
+    audio.play();
+});
+
+document.getElementById('tom3').addEventListener('click', function () {
+    var audio = document.getElementById('audio-tom3');
+    audio.play();
+});
+
+document.getElementById('tom4').addEventListener('click', function () {
+    var audio = document.getElementById('audio-tom4');
+    audio.play();
+});
+
+document.getElementById('bombo').addEventListener('click', function () {
+    var audio = document.getElementById('audio-bombo');
+    audio.play();
+});
+
+document.getElementById('crash').addEventListener('click', function () {
+    var audio = document.getElementById('audio-crash');
+    audio.play();
+});
+
+document.getElementById('hit-hat').addEventListener('click', function () {
+    var audio = document.getElementById('audio-hit-hat');
+    audio.play();
+});
+
+document.getElementById('redoblante').addEventListener('click', function () {
+    var audio = document.getElementById('audio-redoblante');
+    audio.play();
+});
